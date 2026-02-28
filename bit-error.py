3636dcd8c152a70e8e0ad0fc9032b471c75001e7
jgtoves <156796730+jgtoves@users.noreply.github.com>
2026-03-01 02:26:05 +1000

    Update bit-error.py

--- bit-error.py
+++ bit-error.py
@@ -44,8 +44,8 @@ def run_raw_radar():
         s.close()
 
 if __name__ == "__main__":
-    run_raw_radar()                print(f"[!] Binary Shadow Detected: {time.strftime('%H:%M:%S')}")
-            
+    run_raw_radar()
+    
             time.sleep(0.05)
 
     except Exception as e:
